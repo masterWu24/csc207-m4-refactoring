0be50f45be878eda9892da9b9df2a6e9bf161eea
masterWu24 <siha.wu@mail.utoronto.ca>
2025-11-18 02:04:38 -0500

    Task 2.2: extract getVolumeCredits helper

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -27,6 +27,7 @@ public class StatementPrinter {
 
     /**
      * Returns a formatted statement for this invoice.
+     *
      * @return the formatted statement
      * @throws RuntimeException if a play type is unknown
      */
@@ -37,27 +38,23 @@ public class StatementPrinter {
 
         final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (Performance p : invoice.getPerformances()) {
-            final Play play = plays.get(p.getPlayID());
-            final int thisAmount = getAmount(p, play);
+        for (Performance performance : invoice.getPerformances()) {
+            final Play play = plays.get(performance.getPlayID());
+            final int thisAmount = getAmount(performance, play);
 
-            volumeCredits += Math.max(
-                    p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-            if ("comedy".equals(play.getType())) {
-                volumeCredits += p.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
-            }
+            volumeCredits += getVolumeCredits(performance, play);
 
-            result += String.format("  %s: %s (%s seats)\n",
+            result += String.format("  %s: %s (%s seats)%n",
                     play.getName(),
                     frmt.format(thisAmount / Constants.PERCENT_FACTOR),
-                    p.getAudience());
+                    performance.getAudience());
 
             totalAmount += thisAmount;
         }
 
-        result += String.format("Amount owed is %s\n",
+        result += String.format("Amount owed is %s%n",
                 frmt.format(totalAmount / Constants.PERCENT_FACTOR));
-        result += String.format("You earned %s credits\n", volumeCredits);
+        result += String.format("You earned %s credits%n", volumeCredits);
         return result;
     }
 
@@ -68,7 +65,8 @@ public class StatementPrinter {
                 result = Constants.TRAGEDY_BASE_AMOUNT;
                 if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
                     result += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                            * (performance.getAudience()
+                            - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
                 }
                 break;
             case "comedy":
@@ -76,13 +74,27 @@ public class StatementPrinter {
                 if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     result += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
                             + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                            * (performance.getAudience()
+                            - Constants.COMEDY_AUDIENCE_THRESHOLD));
                 }
-                result += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
+                result += Constants.COMEDY_AMOUNT_PER_AUDIENCE
+                        * performance.getAudience();
                 break;
             default:
-                throw new RuntimeException(String.format("unknown type: %s", play.getType()));
+                throw new RuntimeException(
+                        String.format("unknown type: %s", play.getType()));
         }
         return result;
+    }
+
+    private static int getVolumeCredits(Performance performance, Play play) {
+        int volumeCredits =
+                Math.max(performance.getAudience()
+                        - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        if ("comedy".equals(play.getType())) {
+            volumeCredits += performance.getAudience()
+                    / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return volumeCredits;
     }
 }
